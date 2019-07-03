232c4d0b6d5618f9c95538f5981d56f3618228bb
Junyuan Feng <2036293523@qq.com>
2019-07-03 21:14:19 +0800

    update statistic save method

--- bili.py
+++ bili.py
@@ -7,8 +7,10 @@ import time
 import subprocess
 import requests
 import random
+import sqlite3
+import linecache
 
-def LoadUserAgents(uafile):
+def loadUserAgents(uafile):
     uas = []
     with open(uafile, 'rb') as uaf:
         for ua in uaf.readlines():
@@ -17,7 +19,7 @@ def LoadUserAgents(uafile):
     random.shuffle(uas)
     return uas
 
-uas = LoadUserAgents("user_agents.txt")
+uas = loadUserAgents("user_agents.txt")
 
 def newheader():
     head = {
@@ -29,44 +31,51 @@ def newheader():
        'Accept-Language': 'zh-CN,zh;q=0.8'
     }
     return head
-def save(text):
-      path = os.path.join('regex', 'bili-view')
-      with open(path, 'a+') as f:
-           f.write(text)       
+def save(av, title, count):
+      con = sqlite3.connect('regex/bili.db')
+      cur = con.cursor()
+      cur.execute('INSERT INTO view VALUES (%d,"%s",%d)'%(av, title, count))
+      con.commit()
+      con.close()    
 
 def record(av):
       path4history = os.path.join('regex', 'bili-his')
-      with open(path4history, 'a+') as history:
+      with open(path4history, 'w') as history:
            history.write(str(av))
 
-def visit(url):
-      ID = 0
-      for aid in range(10058254,11000000):
+def get():
+    first = linecache.getline('regex/bili-his', 1)
+    if first:
+       return int(first) + 1
+    else:
+       return 1
+
+start = get()
+
+def visit(url):     
+      for aid in range(start,100000):
           try:
               res = requests.get(url + str(aid) + '.html', headers = newheader())
               page = res.content
+              page = page.decode('utf-8')
               re4title = r'(?<="title" content=").*(?=">\n<meta name="keywords")'
               title = re.findall(re4title, page, re.S|re.M)
               re4all = r'(?<="stat":\{"aid":).*(?=,"danmaku")'
               result = re.findall(re4all, page, re.S|re.M)
               if result:
+                   record(aid)
                    re4view = r'(?<="view":).*'
                    view = re.findall(re4view, result[0], re.S|re.M)
                    if int(view[0]) >= 100000:
-                          string = 'Video ID:' + str(aid) + '\n' + 'Title:' + title[0] +'\n' + 'View:' + view[0] + '\n\n'
-                          save(string)
-                          if(aid - ID > 100):
-                             record(aid)
-                             ID = aid
-                          print '\n\nNew Specific Video Found!'
-                          print string
+                          save(aid, title[0], int(view[0]))
+                          print('\n\n' + 'AV:' + str(aid) + '\n' + 'Title:' + title[0] + '\n' + 'Count:' + view [0] + '\n\n')
                    else:
-                          print 'Video ID:' + str(aid) + ' is not specific.'
+                          print('AV:' + str(aid) + ' is not specific.')
               else:
-                    print 'Video ID:' + str(aid) + ' has been deleted.'
+                    print('AV:' + str(aid) + ' has been deleted.')
               time.sleep(0.233)
           except (requests.exceptions.ChunkedEncodingError, requests.ConnectionError) as e:
-              print 'an error occurred.'
+              print('an error occurred.')
 
 if __name__ == '__main__':
       visit('https://m.bilibili.com/video/av')
